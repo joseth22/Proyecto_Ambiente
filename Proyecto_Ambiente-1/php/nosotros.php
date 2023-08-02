95ea88115a645d6f79420fa7141c112040dcaa2a
Joseth222 <107093649+joseth22@users.noreply.github.com>
2023-08-02 10:05:49 -0600

    Pagina principal

---
+++ Proyecto_Ambiente-1/php/nosotros.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nosotros - Venta de Entradas</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Asegúrate de tener el archivo styles.css en el mismo directorio -->
+</head>
+<body>
+    <!-- Barra de navegación -->
+    <nav class="navbar">
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="catalogo.php">Catálogo de Películas</a></li>
+            <li><a href="novedades.php">Novedades</a></li>
+            <li><a href="nosotros.php">Nosotros</a></li>
+        </ul>
+    </nav>
+
+    <!-- Contenido de la página Nosotros -->
+    <div class="content">
+        <h1>Nosotros</h1>
+        <p>Bienvenido a nuestro sitio web de venta de entradas Cine. Somos una plataforma que te permite reservar y adquirir entradas de películas</p>
+        <p>Nuestra misión es brindarte una experiencia fácil y conveniente para que puedas disfrutar de tus de tus peliculas favoritos sin complicaciones. Trabajamos con salas para ofrecerte una amplia selección, en diferentes ubicaciones y horarios.</p>
+        <p>En nuestro catálogo, encontrarás una lista completa de las peliculas disponibles, junto con información detallada sobre cada uno. Puedes ver la sinopsis de películas, y detalles de ubicación y horarios para asegurarte de tomar la mejor decisión para tus planes de entretenimiento.</p>
+        <p>Gracias por confiar en nosotros para tu entretenimiento. Si tienes alguna pregunta o necesitas asistencia, no dudes en contactarnos a través de nuestra sección de contacto. ¡Esperamos que disfrutes tu experiencia en nuestro sitio y que tengas un gran momento con la pelicula que elijas!</p>
+    </div>
+</body>
+</html>
