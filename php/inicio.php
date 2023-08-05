7ea8972c1f9a61a421b5474a4c16986fc0edd6e6
nazznmv <nazznmv241020@gmail.com>
2023-08-05 15:43:04 -0600

    mejora del navbar en inicio

--- php/inicio.php
+++ php/inicio.php
@@ -15,17 +15,18 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="inicio.php">
-                    <img src="img/paisaje3.png" alt="" width="30" height="24">
+                    <img src="../img/Logo.png" alt="Logo" width="175px" height="125px">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Pricing</a>
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a class="nav-link" href="inicio.php">Inicio</a>
+                        <a class="nav-link" href="nosotros.php">Nosotros</a>
+                        <a class="nav-link" href="rutas.php">Rutas</a>
+                        <a class="nav-link" href="horarios.php">Horarios</a>
+                        <a class="nav-link" href="contacto.php">Contáctenos</a>
                     </div>
                 </div>
             </div>
