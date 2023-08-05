4288a515c85c5670a5de2b713b665d966e88e67b
nazznmv <nazznmv241020@gmail.com>
2023-08-05 15:45:58 -0600

    .

--- php/inicio.php
+++ php/inicio.php
@@ -15,7 +15,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="inicio.php">
-                    <img src="../img/Logo.png" alt="Logo" width="175px" height="125px">
+                    <img src="../img/Logo.png" alt="Logo" width="180px" height="125px">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
