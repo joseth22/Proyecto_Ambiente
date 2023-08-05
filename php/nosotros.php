869f5a53d0c7d3a889344a48cd10f4daf86230c7
Joseth222 <107093649+joseth22@users.noreply.github.com>
2023-08-05 11:28:13 -0600

    Nosotros

--- php/nosotros.php
+++ php/nosotros.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
 <html>
     <head>
-            <meta>
-            <title>
-
-            </title>
-            <link>
-    </head>
+		<title>Nosotros</title>
+        <link rel="stylesheet" href="style.css">
+	</head>
     <body>
+	
+        <div class="Nosotros">
+            <h2>¿Quienes somos?</h2>
+                <p>Somos una empresa nos comprometemos a brindar un excelente servicio de transporte público y tener nuestras
+                unidades de la mejor manera, para poder brindales a todos nustros usuarios  un buen servicio seguro y de
+                calidad, contamos con diferentes rutas para el traslado de los habitantes, nuestro choferes estan 
+                altamente capacitado.
         
+                Si necesitas contactar a la empre para conseguir alguna información especial, puedes enviar un correo
+                a busesblueline@service.ac.cr o llamar al telefono: 2590-7856. </p>
+        
+            <h2>Visión</h2>
+                <p>Ser la empresa de referencia del sector del transporte y la logística gracias al compromiso, implicación y los servicios flexibles y ajustados a las necesidades de los clientes para poder transportar cualquier tipo de mercancía creando un vínculo a largo plazo con estos clientes.</p>
+
+            <h2>Misión</h2>
+                <p>Proporcionar los servicios de transporte y logística en el ámbito nacional e internacional de una manera profesional, cubriendo las necesidades de los clientes y proporcionando un trato personalizado para que cumplan sus objetivos.</p>
     </body>
 </html>
