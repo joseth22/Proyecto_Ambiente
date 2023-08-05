72e6dce77cf4e59fa59e007c7b67aa21a6f04eb5
nazznmv <nazznmv241020@gmail.com>
2023-08-05 15:04:09 -0600

    Cambios en nosotros

--- php/nosotros.php
+++ php/nosotros.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
-<html>
-    <head>
-		<title>Nosotros</title>
-        <link rel="stylesheet" href="style.css">
-	</head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nosotros</title>
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/nosotros.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
     <body>
 	
         <div class="Nosotros">
+
             <h2>¿Quienes somos?</h2>
+            <div class="quienes">
                 <p>Somos una empresa nos comprometemos a brindar un excelente servicio de transporte público y tener nuestras
                 unidades de la mejor manera, para poder brindales a todos nustros usuarios  un buen servicio seguro y de
                 calidad, contamos con diferentes rutas para el traslado de los habitantes, nuestro choferes estan 
@@ -15,11 +22,33 @@
         
                 <p>Si necesitas contactar a la empre para conseguir alguna información especial, puedes enviar un correo
                 a busesblueline@service.ac.cr o llamar al telefono: 2590-7856. </p>
-        
-            <h2>Visión</h2>
-                <p>Ser la empresa de referencia del sector del transporte y la logística gracias al compromiso, implicación y los servicios flexibles y ajustados a las necesidades de los clientes para poder transportar cualquier tipo de mercancía creando un vínculo a largo plazo con estos clientes.</p>
+            </div>
+            
+            <div class="vis-mis">
 
-            <h2>Misión</h2>
-                <p>Proporcionar los servicios de transporte y logística en el ámbito nacional de una manera profesional, cubriendo las necesidades de los clientes y proporcionando un trato personalizado para que cumplan sus objetivos.</p>
+                <h2>Visión</h2>
+                <div class="vision">
+                    <p>Ser la empresa de referencia del sector del transporte y la logística gracias al compromiso, implicación y los servicios flexibles y ajustados a las necesidades de los clientes para poder transportar cualquier tipo de mercancía creando un vínculo a largo plazo con estos clientes.</p>
+                </div>
+
+                <h2>Misión</h2>
+                <div class="mision">
+                    <p>Proporcionar los servicios de transporte y logística en el ámbito nacional de una manera profesional, cubriendo las necesidades de los clientes y proporcionando un trato personalizado para que cumplan sus objetivos.</p>
+                </div>
+            </div>
+        </div>
+            
+          
+    <!--JS Bootstrap-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    
     </body>
 </html>
