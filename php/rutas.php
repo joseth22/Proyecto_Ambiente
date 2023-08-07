e6cc44a2df2992fe71a8b4633092073c42e1483f
Joseth222 <107093649+joseth22@users.noreply.github.com>
2023-08-07 00:01:28 -0600

    Avance en rutas

--- php/rutas.php
+++ php/rutas.php
@@ -5,16 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rutas</title>
     <link rel="stylesheet" href="../css/normalize.css">
-    <link rel="stylesheet" href="../css/nosotros.css">
+    <link rel="stylesheet" href="../css/rutas.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
     <body>
 
     <?php include 'header.php'; ?>
+<!--Sección de rutas-->
+<div>
+        <h1 class="h1">Rutas</h1>
 
+        <div class="contenedor1">
+            <div class="card" style="width: 18rem;">
+                <img src="../img/rutacar.jpg" alt="...">
+                <div class="card-body">
+                    <p class="card-text"><h3>Cartago</h3>100 mestros norte de la estacion de trenes.</p>
+                    
+                </div>
+            </div>
+
+            <div class="contenedor2">
+            <div class="card" style="width: 18rem;">
+                <img src="../img/rutaguana.jpg" alt="...">
+                <div class="card-body">
+                    <p class="card-text"><h3>Guanacaste</h3>Al frente del centro comercial de Liberia.</p>
+                </div>
+            </div>
+
+            <div class="contenedor3">
+            <div class="card" style="width: 18rem;">
+                <img src="../img/rutasan.jpg" alt="...">
+                <div class="card-body">
+                    <p class="card-text"><h3>San Jose</h3>250 metros de la coca cola.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php include 'footer.php'; ?>
-        
+            <!--JS Bootstrap-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
     </body>
 </html>
